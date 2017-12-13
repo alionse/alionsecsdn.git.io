ecfaffdd84950d657b905d7f661cb9bc3c2513f8
alionse <381318242@qq.com>
2017-12-13 13:48:22 +0800

    测试代码提交

---
+++ itcast-javaweb/src/com/xml01/Test.java
@@ -0,0 +1,16 @@
+package com.xml01;
+/**
+*
+*<P>Title:Test.java</P>
+*<P>Description</P>
+*<P>Company:com.alionse</P>
+* @author Alionse  
+* @date 创建时间：Dec 13, 2017 1:46:40 PM
+* @version 1.0
+* @parameter 
+*/
+public class Test {
+	public static void main(String []args){
+		System.out.println("测试服务器");
+	}
+}
